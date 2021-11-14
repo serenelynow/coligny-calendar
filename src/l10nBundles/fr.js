2b34218886711759c138e6541137dbc32dd79e13
serenelynow <jaynna@hotmail.com>
2021-11-14 07:20:13 -0600

    Adding French language bundle

---
+++ src/l10nBundles/fr.js
@@ -0,0 +1,14 @@
+export const fr = {
+	about: "À propos",
+	aboutDesc: "Ce calendrier a été développé sur la base des recherches d'{0} sur la tablette de pierre calendaire trouvée à Coligny, France en 1897. Ce calendrier est basé sur un cycle solilunaire métonique de 19 ans. Les journées commencent à 18 heures, heure locale, elles s'étendent donc sur deux dates grégoriennes.",
+	bce: "AEC",
+	ce: "EC",
+	close: "Fermer",
+	colignyCalendar: "Le Calendrier de Coligny",
+	goto: "Aller à",
+	gototoday: "Aller à aujourd'hui",
+	month: "Mois",
+	today: "Aujourd'hui",
+	todayis: "Nous sommes le",
+	year: "Année"
+};
