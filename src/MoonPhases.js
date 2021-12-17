6d7490fc353e889325e93adf297370a77b5eb5fc
serenelynow <jaynna@hotmail.com>
2021-12-17 12:30:18 -0600

    Accommodating for the calendar to load without moon phases if there is no internet connection.

--- src/MoonPhases.js
+++ src/MoonPhases.js
@@ -6,23 +6,23 @@
 
 const moonPhases = {};
 
-function loadMoonPhases(otherData, params,callback){
+function loadMoonPhases(otherData, params, callback){
     if (moonPhases[otherData.cYear.toString()] != undefined && moonPhases[otherData.cYear.toString()][otherData.cMonth.toString()] != undefined) {
         // return the existing
         callback(moonPhases[otherData.cYear.toString()][otherData.cMonth.toString()]);
-    } else {
+    } else if (navigator.onLine == true) {
         // go fetch 
-        var gets=[]
-        for (var i in params){
-            gets.push(i + "=" +encodeURIComponent(params[i]))
+        var gets = [];
+        for (var i in params) {
+            gets.push(i + "=" + encodeURIComponent(params[i]));
         }
-        var xmlhttp = new XMLHttpRequest()
-        var url = "https://www.icalendar37.net/lunar/api/?" + gets.join("&")
+        var xmlhttp = new XMLHttpRequest();
+        var url = "https://www.icalendar37.net/lunar/api/?" + gets.join("&");
         xmlhttp.onreadystatechange = function() {
             if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
                 var json;
                 try {
-                     json = JSON.parse(xmlhttp.responseText)
+                     json = JSON.parse(xmlhttp.responseText);
                 } catch (e) {
                     console.warn('An unexpected response occurred retrieving the moonPhases.');
                     console.warn('responseText: ' + xmlhttp.responseText);
@@ -33,8 +33,16 @@ function loadMoonPhases(otherData, params,callback){
                 
             }
         }
-        xmlhttp.open("GET", url, true)
-        xmlhttp.send()
+        xmlhttp.ontimeout = function (e) {
+            console.warn("Loading moon phases is taking too long and so won't be loaded for this month");
+            loadNewPhases(otherData, undefined, callback);
+        };
+        xmlhttp.timeout = 5000;
+        xmlhttp.open("GET", url, true);
+        xmlhttp.send();
+    } else {
+        console.warn("There is no internet connection so the moon phases won't be loaded.");
+        loadNewPhases(otherData, undefined, callback);
     }
 }
 
